c823b2bdc5885c31a460e91feefcf935f0afb38d
wangyong <1558448539@qq.com>
2019-03-26 00:04:39 +0800

    我的第一次提交

--- 3/1.py
+++ 3/1.py
@@ -3,14 +3,11 @@ from dateutil import parser
 from datetime import datetime
 import re
 rule1= '(.*?)年(.*?)月(.*?)日'
+rule2 = r'//.*?;.*?/\*[\s\S]*?\*/.*?(?=\\r\\n)'
+c2 = re.compile(rule2)
 c1 = re.compile(rule1)
-try:
-    time_str = "2018.9.3"
-    match =  c1.search(time_str)
-    if match:
-        time_str = match.group(1)+"-"+match.group(2)+"-"+match.group(3)
-    dtime = parser.parse(time_str)
-    str = dtime.strftime("%Y-%m-%d")
-    print(str)
-except:
-    print(time_str)
+print(c2)
+str = r"// this is my;  /* this is a test */       \r\n"
+print(c2.search(str))
+
+

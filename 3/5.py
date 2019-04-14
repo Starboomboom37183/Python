93fbe6885646ff09eaabd4b6ae9580799aa9c359
wangyong <1558448539@qq.com>
2019-04-14 14:50:14 +0800

    括号换行参数对齐-最终版

--- 3/5.py
+++ 3/5.py
@@ -8,29 +8,27 @@ end = 0
 i = 0
 output = open("Dijkstra.c","w")
 while i<len(lines):
-    if lines[i]=="(":
+    if lines[i]=="{":
         if count ==0:
             start = end = i
         count+=1
-    elif lines[i]==")":
+    elif lines[i]=="}":
         if count==0:
             break
         count-=1
         if count==0:
             end = i+1
             temp = lines[start:end].splitlines()
+            pos= lines.rfind("\n",0,start)
+            length = start - pos
             if len(temp)>1:
-                pos = lines.rfind("\n", 0, start)
-                for k in range(start + 1, end):
-                    if lines[k] != " ":
-                        break
-                length = k - pos - 1
+                print(temp)
                 line_replace = ""
                 for i in range(1,len(temp)):
                     temp[i]=" "*length+temp[i].lstrip()
                 for s in temp:
                     line_replace = line_replace+s+"\n"
-                lines = lines[:start]+line_replace.rstrip()+lines[end:]
+                lines = lines[:start]+line_replace+lines[end:]
                 i = start + len(line_replace)+1
 
     i+=1

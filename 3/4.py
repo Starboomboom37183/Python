8346c5ef7dc6806c54ddf15fb70d6ec13c853ca8
wangyong <1558448539@qq.com>
2019-04-14 14:33:58 +0800

    括号实验

--- 3/4.py
+++ 3/4.py
@@ -1,25 +1,40 @@
 # -*- coding: utf-8 -*-
 input = open("Dijkstra.c","r")
 lines = input.read()
+input.close()
 count = 0
 start = 0
 end = 0
 i = 0
+#output = open("Dijkstra.c","w")
 while i<len(lines):
     if lines[i]=="(":
         if count ==0:
             start = end = i
         count+=1
     elif lines[i]==")":
-        if count!=0:
-            count-=1
-            if count==0:
-                end = i+1
-        else:
-            print("error")
-    if count==0 and start!=end:
-        str1 = lines[start:end]
+        if count==0:
+            break
+        count-=1
+        if count==0:
+            end = i+1
+            print(lines[start:end])
+            temp = lines[start:end].splitlines()
+            pos= lines.rfind("\n",0,start)
+            length = start - pos
+            print(temp)
+            if len(temp) == 1:
+                continue
+            else:
+                line_replace = ""
+                for i in range(1,len(temp)):
+                    temp[i]=" "*length+temp[i].lstrip()
+                for s in temp:
+                    line_replace = line_replace+s+"\n"
+                lines = lines[:start]+line_replace+lines[end:]
+                i = start + len(line_replace)+1
 
     i+=1
-
+#output.write(lines)
+#output.close()
 
